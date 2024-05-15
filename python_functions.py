6a6b95210bd0f7be371c376253f6903c19c76416
ChaseHarding <chaseharding965@gmail.com>
2024-05-14 21:39:48 -0700

    3rd question

--- python_functions.py
+++ python_functions.py
@@ -1,29 +1,38 @@
-# Hello there
+# # Hello there
 
-# Write a Python function called max_num()to find the Max of three numbers.
-def max_num(a,b,c):
-  return max([a,b,c])
+# # Write a Python function called max_num()to find the Max of three numbers.
+# def max_num(a,b,c):
+#   return max([a,b,c])
 
-print(max_num(1,2,3))
-print(max_num(100,50,1))
-print(max_num(15,30,2))
+# print(max_num(1,2,3))
+# print(max_num(100,50,1))
+# print(max_num(15,30,2))
 
-# Write a Python function called mult_list()  to multiply all the numbers in a list.
+# # Write a Python function called mult_list()  to multiply all the numbers in a list.
 
-def mult_list(lst):
-  #if empty list, return 0
-  if len(lst) == 0:
-    return 0
-  #product starts with first element of list
-  prod = lst[0]
+# def mult_list(lst):
+#   #if empty list, return 0
+#   if len(lst) == 0:
+#     return 0
+#   #product starts with first element of list
+#   prod = lst[0]
   
-    #go through list elements and multiply them together
-  if len(lst) > 1:
-    for i in lst[1:]:
-      prod = prod * i
+#     #go through list elements and multiply them together
+#   if len(lst) > 1:
+#     for i in lst[1:]:
+#       prod = prod * i
 
-  return prod
+#   return prod
     
-print(mult_list([1,2,3]))
-print(mult_list([]))
-print(mult_list([15]))
+# print(mult_list([1,2,3]))
+# print(mult_list([]))
+# print(mult_list([15]))
+
+# Write a Python function called rev_string() to reverse a string.
+
+def rev_string(my_str):
+  return my_str[::-1]
+
+print(rev_string(""))
+print(rev_string("apple"))
+print(rev_string("mt string"))
