c57360b1052d217bf13dfd225b143e2fa740af1d
ChaseHarding <chaseharding965@gmail.com>
2024-05-14 21:37:01 -0700

    2nd question

--- python_functions.py
+++ python_functions.py
@@ -7,3 +7,23 @@ def max_num(a,b,c):
 print(max_num(1,2,3))
 print(max_num(100,50,1))
 print(max_num(15,30,2))
+
+# Write a Python function called mult_list()  to multiply all the numbers in a list.
+
+def mult_list(lst):
+  #if empty list, return 0
+  if len(lst) == 0:
+    return 0
+  #product starts with first element of list
+  prod = lst[0]
+  
+    #go through list elements and multiply them together
+  if len(lst) > 1:
+    for i in lst[1:]:
+      prod = prod * i
+
+  return prod
+    
+print(mult_list([1,2,3]))
+print(mult_list([]))
+print(mult_list([15]))
